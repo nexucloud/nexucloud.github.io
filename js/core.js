cfb9b1feae7735af217f51e1ded7b121903b69f3
jdeocampo <jellydeocampo@icloud.com>
2014-06-02 23:14:12 +0800

    Initial commit

---
+++ js/core.js
@@ -0,0 +1,11 @@
+
+	$(window).scroll(function() {
+		$('#animatedElement').each(function(){
+		var imagePos = $(this).offset().top;
+
+		var topOfWindow = $(window).scrollTop();
+			if (imagePos < topOfWindow+400) {
+				$(this).addClass("slideUp");
+			}
+		});
+	});
